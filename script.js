e7f6dc500321ba8525001d87990802d67fe5ce55
BrunoTdC <104500870+BrunoTdC@users.noreply.github.com>
2022-09-21 16:09:46 -0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-window.sr = ScrollReveal({reset: true})
-
-sr.reveal('.headline',{
-    rotate:{ x:0, y:30, z:0},
-    duration: 2000,})
-
-sr.reveal('.headline2',{
-    rotate:{ x:0, y:30, z:0},
-    duration: 5000,})
