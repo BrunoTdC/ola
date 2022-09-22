6e13a8fd4b0fec1b390a50d6fbb02057fc0bff83
BrunoTdC <104500870+BrunoTdC@users.noreply.github.com>
2022-09-22 15:53:35 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-window.sr = ScrollReveal({reset: true})
-
-sr.reveal('.headline',{
-    rotate:{ x:0, y:30, z:0},
-    duration: 2000,})
-
-sr.reveal('.headline2',{
-    rotate:{ x:0, y:30, z:0},
-    duration: 5000,})
 
     let valido = true  
 
